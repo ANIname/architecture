3357fc81d0d63e2e12cdc06a6fbd6fdc33f04f46
KiiDii <kiidii.aniname@gmail.com>
2025-07-12 23:34:32 +0300

    🔧 REFACTOR: update PostgreSQL setup workflow to streamline initialization and connection checks, simplify variable management, and enhance Terraform configuration for improved deployment efficiency

--- terraform/postgresql/main.tf
+++ terraform/postgresql/main.tf
@@ -9,13 +9,10 @@ terraform {
 }
 
 provider "postgresql" {
-  host            = var.db_host
-  port            = 5432
-  database        = "postgres"
-  username        = var.db_admin_user
-  password        = var.db_admin_password
-  sslmode         = "require"
-  connect_timeout = 15
+  host     = var.db_host
+  username = "postgres"
+  password = ""
+  sslmode  = "disable"
 }
 
 resource "postgresql_role" "app_user" {
@@ -25,11 +22,6 @@ resource "postgresql_role" "app_user" {
 }
 
 resource "postgresql_database" "app_db" {
-  name   = var.app_db
-  owner  = postgresql_role.app_user.name
-  encoding = "UTF8"
-  lc_collate = "en_US.UTF-8"
-  lc_ctype   = "en_US.UTF-8"
-  template   = "template0"
-  allow_connections = true
+  name  = var.app_db
+  owner = postgresql_role.app_user.name
 } 
--- terraform/postgresql/variables.tf
+++ terraform/postgresql/variables.tf
@@ -1,6 +1,4 @@
 variable "db_host" {}
-variable "db_admin_user" {}
-variable "db_admin_password" {}
 variable "app_user" {}
 variable "app_password" {}
 variable "app_db" {} 
